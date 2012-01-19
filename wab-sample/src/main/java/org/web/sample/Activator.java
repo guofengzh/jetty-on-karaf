815fc555aa23ca7e6f89ad20aedf91510037ef3d
Guofeng Zhang <guofeng@radvision.com>
2012-01-19 18:41:23 +0800

    custom distribution

---
+++ wab-sample/src/main/java/org/web/sample/Activator.java
@@ -0,0 +1,15 @@
+package org.web.sample;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+ public void start(BundleContext ctx) throws Exception {
+
+ }
+
+ public void stop(BundleContext ctx) throws Exception {
+
+ }
+}
