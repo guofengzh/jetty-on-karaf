d4665033270d4a557eb242dbc782bd6aa45dd3f1
Guofeng Zhang <guofengzh@gmail.com>
2012-10-09 11:18:27 +0800

    changed to use org.ops4j.pax.url/pax-url-aether/1.5.1-SNAPSHOT, which we enchanced to support resolving an artifact referenced with "mvn" to a file in local maven repository

--- jetty-osgi-boot-mvn/src/main/java/org/eclipse/jetty/osgi/boot/utils/FileLocatorHelperImpl.java
+++ jetty-osgi-boot-mvn/src/main/java/org/eclipse/jetty/osgi/boot/utils/FileLocatorHelperImpl.java
@@ -7,7 +7,8 @@ import org.eclipse.jetty.osgi.boot.utils.internal.DefaultFileLocatorHelper;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference ;
-import org.ops4j.pax.url.mvn.MavenArtifactResolverService;
+import org.osgi.service.url.URLConstants;
+import org.ops4j.pax.url.mvn.Mvn2FileResolverService;
 import org.ops4j.pax.url.mvn.ServiceConstants;
 
 public class FileLocatorHelperImpl extends DefaultFileLocatorHelper
@@ -19,11 +20,11 @@ public class FileLocatorHelperImpl extends DefaultFileLocatorHelper
     	if ( loc.startsWith( "mvn:") )
     	{
 			BundleContext bundleContext = bundle.getBundleContext() ;
-			String filter = "(" + MavenArtifactResolverService.URL_RESOLVER_PROTOCOL + "=" + ServiceConstants.PROTOCOL + ")" ;
-			ServiceReference[] references = bundleContext.getServiceReferences(MavenArtifactResolverService.class.getName(), filter);
+			String filter = "(" + URLConstants.URL_HANDLER_PROTOCOL + "=" + ServiceConstants.PROTOCOL + ")" ;
+			ServiceReference[] references = bundleContext.getServiceReferences(Mvn2FileResolverService.class.getName(), filter);
 			if ( references != null && references.length != 0 )
 			{
-				MavenArtifactResolverService resolverService = (MavenArtifactResolverService)bundleContext.getService(references[0]) ;
+				Mvn2FileResolverService resolverService = (Mvn2FileResolverService)bundleContext.getService(references[0]) ;
  			    File f = resolverService.resolve( new URL( loc ) ) ;
 			    bundleContext.ungetService( references[0] ) ;
                 return f ;
